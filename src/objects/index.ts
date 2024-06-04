6cac0335125f3b57f15c9ed9fac824538525c0b5
Sassa1993 <118011618+MiraiTsukiGames@users.noreply.github.com>
2024-06-04 17:53:09 +0200

    progetto

---
+++ src/objects/index.ts
@@ -0,0 +1,6 @@
+import { Coin } from "./coin"
+import { Snake } from "./snake"
+import { FastPlayer } from "./fastplayer"
+import { SlowPlayer } from "./slowplayer"
+
+export { Coin, Snake, FastPlayer, SlowPlayer }
